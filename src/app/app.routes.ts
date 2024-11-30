e0d1801c5aebbbb4485884fda2aaec1e7d054d73
Gefferson Costa <81829446+GeffersonCosta@users.noreply.github.com>
2024-11-30 18:43:26 +0000

    alteracao

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -6,7 +6,7 @@ import { GameDetailsComponent } from "./components/game-details/game-details.com
 import { ProfileComponent } from "./components/profile/profile.component";
 
 export const routes: Routes = [
-  { path: "", redirectTo: "/games", pathMatch: "full" },
+  // { path: '', redirectTo: '/games', pathMatch: 'full' },
   { title: "Home Page", path: "home", component: HomeComponent },
   { title: "Games", path: "games", component: GameListComponent },
   { title: "Game Detail", path: "game/:id", component: GameDetailsComponent },
