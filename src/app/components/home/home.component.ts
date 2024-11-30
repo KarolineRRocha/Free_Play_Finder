a35f6dee7a455fc32c8358ccc7e8f5db62e3be70
MatheusMasago <matheus.masago@hotmail.com>
2024-11-30 19:15:52 +0000

    Filtragens pt1

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -3,17 +3,23 @@ import { Component, OnInit } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
 import { GameService } from './../../services/game.service';
+import { NgFor } from '@angular/common';
+import { NgClass } from '@angular/common';
+import { NgIf } from '@angular/common';
 import { Game } from '../../models/game'
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [RouterLink, RouterLinkActive, RouterOutlet],
+  imports: [RouterLink, RouterLinkActive, RouterOutlet, NgFor, NgIf],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
 export class HomeComponent {
   games: Array<Game> = [];
+  filteredGames: Array<Game> = [];
+  sortBy: string = 'title-asc';
+  searchTerm: string = '';
 
   constructor(private gameService: GameService) { }
 
@@ -23,5 +29,12 @@ export class HomeComponent {
         this.games = data;
       }
     })
+  }
+  onSearch(event: Event) {
+    const target = event.target as HTMLInputElement; // Afirmação de tipo
+    const searchTerm = target.value;
+    this.filteredGames = this.games.filter(game => 
+      game.title.toLowerCase().includes(searchTerm.toLowerCase())
+    );
   }
 }
